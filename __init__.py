af9eebc027306105be1b1166673c7bc8cd8f8d10
Ola Oni <oa.oni7@gmail.com>
2021-10-08 23:16:23 -0400

    add support for built in query methods

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from .modAL.models import ActiveLearner
+from .modAL.models import ActiveCompletion
+from .modAL.utils import active_sample, max_uncertainty, weighted_uncertainty, random_query, leverage_online, leverage_observed
--- modAL/utils/__init__.py
+++ modAL/utils/__init__.py
@@ -2,10 +2,13 @@ from .combination import make_linear_combination, make_product, make_query_strat
 from .data import data_vstack, addData2matrix
 from .selection import multi_argmax, weighted_random
 from .validation import check_class_labels, check_class_proba
+from .query import active_sample, max_uncertainty, weighted_uncertainty, random_query, leverage_online, leverage_observed
 
 __all__ = [
     'make_linear_combination', 'make_product', 'make_query_strategy',
     'data_vstack','addData2matrix',
     'multi_argmax', 'weighted_random',
-    'check_class_labels', 'check_class_proba'
+    'check_class_labels', 'check_class_proba',
+    'active_sample', 'max_uncertainty','weighted_uncertainty','random_query',
+    'leverage_online', 'leverage_observed'
 ]
